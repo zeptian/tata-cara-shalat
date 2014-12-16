35a7529070923122102833249e42465c3c8d0dd2
septian <boye.septian@gmail.com>
2014-12-16 22:31:34 +0700

    comit1

---
+++ setting.js
@@ -0,0 +1,7 @@
+//window.disableTapAdvanceSlide = true;
+//window.disableTapAdvanceAnimation = true;
+//window.overrideTransition = {ef:3853,t:2};
+//window.disableSwipe = true;
+//window.disableFullscreen = true
+//window.exactFit = true
+//window.loopUntilEsc = true
